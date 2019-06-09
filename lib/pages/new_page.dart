6656fafa0fa2fb096ea8d8bed21eeb26f706a8ed
Glen Muras <gmuras@Glens-MBP.exp.local>
2019-06-09 22:11:54 +1000

    Added the text input fields.

--- lib/pages/new_page.dart
+++ lib/pages/new_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+import 'package:intl/intl.dart';
 
 class NewPage extends StatefulWidget {
   @override
@@ -16,16 +19,10 @@ class _NewPageState extends State<NewPage> {
   // Helper functions.
 
   // **************************************************************** //
-  // Action functions.
-
-  // **************************************************************** //
-  // Convert decimal Number area.
-
-  // **************************************************************** //
   // Screen creation area.
   Widget createBody(BuildContext context) {
     return Container(
-      color: Colors.lightBlueAccent,
+      color: Colors.lightBlue[50],
       child: SafeArea(
         top: false,
         bottom: false,
@@ -37,16 +34,105 @@ class _NewPageState extends State<NewPage> {
             children: <Widget>[
               new TextFormField(
                 decoration: InputDecoration(
-                  icon: const Icon(Icons.date_range),
+                  icon: const Icon(Icons.calendar_today),
                   hintText: 'Enter the date.',
                   labelText: 'Date',
                 ),
-              )
+                controller: _controller,
+                keyboardType: TextInputType.datetime,
+              ),
+              new TextFormField(
+                decoration: InputDecoration(
+                  icon: const Icon(Icons.timer),
+                  hintText: 'Enter the time.',
+                  labelText: 'Time',
+                ),
+              ),
+              new TextFormField(
+                decoration: InputDecoration(
+                  icon: const Icon(Icons.favorite_border),
+                  hintText: 'Enter Systolic value.',
+                  labelText: 'Systolic',
+                ),
+                keyboardType: TextInputType.phone,
+                inputFormatters: [
+                  WhitelistingTextInputFormatter.digitsOnly,
+                ],
+              ),
+              new TextFormField(
+                decoration: InputDecoration(
+                  icon: const Icon(Icons.favorite_border),
+                  hintText: 'Enter Diastolic value.',
+                  labelText: 'Diastolic',
+                ),
+                keyboardType: TextInputType.phone,
+                inputFormatters: [
+                  WhitelistingTextInputFormatter.digitsOnly,
+                ],
+              ),
+              new TextFormField(
+                decoration: InputDecoration(
+                  icon: const Icon(
+                    Icons.favorite,
+                    color: Colors.redAccent,
+                  ),
+                  hintText: 'Enter the Heart rate.',
+                  labelText: 'Heart Rate',
+                ),
+                keyboardType: TextInputType.phone,
+                inputFormatters: [
+                  WhitelistingTextInputFormatter.digitsOnly,
+                ],
+              ),
+              new TextFormField(
+                decoration: InputDecoration(
+                  icon: const Icon(Icons.note_add),
+                  hintText: 'Enter a note.',
+                  labelText: 'Note',
+                ),
+              ),
+              new Container(
+                padding: const EdgeInsets.only(left: 40.0, top: 20.0),
+                child: new RaisedButton(
+                  child: const Text('Submit'),
+                  onPressed: null,
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  final TextEditingController _controller = new TextEditingController();
+  Future _chooseDate(BuildContext context, String initialDateString) async {
+    var now = new DateTime.now();
+    var initialDate = convertToDate(initialDateString) ?? now;
+    initialDate = (initialDate.year >= 1900 && initialDate.isBefore(now)
+        ? initialDate
+        : now);
+
+    var result = await showDatePicker(
+        context: context,
+        initialDate: initialDate,
+        firstDate: new DateTime(1900),
+        lastDate: new DateTime.now());
+
+    if (result == null) return;
+
+    setState(() {
+      _controller.text = new DateFormat.yMd().format(result);
+    });
+  }
+
+  DateTime convertToDate(String input) {
+    try {
+      var d = new DateFormat.yMd().parseStrict(input);
+      return d;
+    } catch (e) {
+      return null;
+    }
   }
 
   @override
